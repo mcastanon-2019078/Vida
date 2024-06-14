1c416ed805dac90d65d42643e43d77220f61e360
wcbarera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-06-14 16:06:33 -0600

    cambiando las rutas de la base de datos

--- VIDA-NodeJS/configs/mongo.js
+++ VIDA-NodeJS/configs/mongo.js
@@ -14,7 +14,7 @@ export const connect = async()=>{
         mongoose.connection.on('disconnected', ()=> console.log('MongoDB | disconnected'))
         mongoose.connection.on('reconnected', ()=> console.log('MongoDB | reconnected to mongodb'))
 
-        return await mongoose.connect(process.env.MONGO_URI)
+        return await mongoose.connect('mongodb+srv://BugBusters:b8czC5CttmLHC38s@clustervida.jx4uihv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterVIDA')
     } catch (err) {
         console.error('Database connection failed', err)
         
